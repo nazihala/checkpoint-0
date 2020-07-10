846728129834ade306c1a7a84e27cf22a2348feb
nazihala <67385296+nazihala@users.noreply.github.com>
2020-07-10 14:14:58 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -5,43 +5,28 @@ import Form from 'react-bootstrap/Form';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Form>
+  <Form>
   <Form.Group controlId="exampleForm.ControlInput1">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="name@example.com" />
+    <Form.Label>E-mail</Form.Label>
+    <Form.Control type="email" placeholder="tapez votre email" />
   </Form.Group>
   <Form.Group controlId="exampleForm.ControlSelect1">
     <Form.Label>Example select</Form.Label>
     <Form.Control as="select">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
+      <option>HTML</option>
+      <option>CSS</option>
+      <option>JS</option>
+      <option>REACT JS</option>
     </Form.Control>
   </Form.Group>
   <Form.Group controlId="exampleForm.ControlSelect2">
     <Form.Label>Example multiple select</Form.Label>
     <Form.Control as="select" multiple>
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
+      <option>Html</option>
+      <option>CSS</option>
+      <option>ES5</option>
+      <option>ES6</option>
+      <option>React Js</option>
     </Form.Control>
   </Form.Group>
   <Form.Group controlId="exampleForm.ControlTextarea1">
